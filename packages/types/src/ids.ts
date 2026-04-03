9ebe66884ac5790c3b42ac2f7378e3e64e525137
Cesare Naldi <3353250+cesarenaldi@users.noreply.github.com>
2026-04-03 14:54:41 +0200

    fix: remove unrelated ids formatting

--- packages/types/src/ids.ts
+++ packages/types/src/ids.ts
@@ -18,10 +18,7 @@ export type ChatId = Tagged<string, 'ChatId'>;
 export type ClobRewardId = Tagged<string, 'ClobRewardId'>;
 export type CollectionId = Tagged<string, 'CollectionId'>;
 export type EventCreatorId = Tagged<string, 'EventCreatorId'>;
-export type EventExternalPartnerMappingId = Tagged<
-  number,
-  'EventExternalPartnerMappingId'
->;
+export type EventExternalPartnerMappingId = Tagged<number, 'EventExternalPartnerMappingId'>;
 export type EventId = Tagged<string, 'EventId'>;
 export type ImageOptimizationId = Tagged<string, 'ImageOptimizationId'>;
 export type InternalUserId = Tagged<string, 'InternalUserId'>;
@@ -37,8 +34,7 @@ export const toBestLineId = (value: string): BestLineId =>
   toTaggedString<BestLineId>(value);
 export const toCategoryId = (value: string): CategoryId =>
   toTaggedString<CategoryId>(value);
-export const toChatId = (value: string): ChatId =>
-  toTaggedString<ChatId>(value);
+export const toChatId = (value: string): ChatId => toTaggedString<ChatId>(value);
 export const toClobRewardId = (value: string): ClobRewardId =>
   toTaggedString<ClobRewardId>(value);
 export const toCollectionId = (value: string): CollectionId =>
@@ -47,10 +43,8 @@ export const toEventCreatorId = (value: string): EventCreatorId =>
   toTaggedString<EventCreatorId>(value);
 export const toEventExternalPartnerMappingId = (
   value: number,
-): EventExternalPartnerMappingId =>
-  toTaggedInteger<EventExternalPartnerMappingId>(value);
-export const toEventId = (value: string): EventId =>
-  toTaggedString<EventId>(value);
+): EventExternalPartnerMappingId => toTaggedInteger<EventExternalPartnerMappingId>(value);
+export const toEventId = (value: string): EventId => toTaggedString<EventId>(value);
 export const toImageOptimizationId = (value: string): ImageOptimizationId =>
   toTaggedString<ImageOptimizationId>(value);
 export const toInternalUserId = (value: string): InternalUserId =>
@@ -61,10 +55,8 @@ export const toPartnerId = (value: number): PartnerId =>
   toTaggedInteger<PartnerId>(value);
 export const toSeriesId = (value: string): SeriesId =>
   toTaggedString<SeriesId>(value);
-export const toSportId = (value: number): SportId =>
-  toTaggedInteger<SportId>(value);
+export const toSportId = (value: number): SportId => toTaggedInteger<SportId>(value);
 export const toTagId = (value: string): TagId => toTaggedString<TagId>(value);
-export const toTeamId = (value: number): TeamId =>
-  toTaggedInteger<TeamId>(value);
+export const toTeamId = (value: number): TeamId => toTaggedInteger<TeamId>(value);
 export const toTemplateId = (value: string): TemplateId =>
   toTaggedString<TemplateId>(value);
