e081643bbdf3491ed93da74b58c15d19e7ce74ef
Cesare Naldi <3353250+cesarenaldi@users.noreply.github.com>
2026-05-06 20:18:20 +0200

    test(client): move series integration tests

--- packages/client/tests/integration/series.test.ts
+++ packages/client/tests/integration/series.test.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from 'vitest';
-import { expectNonEmptyPage, publicClient } from '../testing';
+import { describe, expect, it } from './fixtures';
+import { expectNonEmptyPage } from './helpers';
 
 describe('Series', () => {
   describe('listSeries', () => {
-    it('fetches series', async () => {
+    it('fetches series', async ({ publicClient }) => {
       const result = await publicClient
         .listSeries({
           pageSize: 1,
@@ -21,7 +21,7 @@ describe('Series', () => {
   });
 
   describe('fetchSeries', () => {
-    it('fetches a series by id', async () => {
+    it('fetches a series by id', async ({ publicClient }) => {
       const {
         items: [series],
       } = await publicClient
